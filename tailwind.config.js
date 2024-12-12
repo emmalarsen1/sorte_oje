bf5978013485b9354a85ae77b8a1eca1f28156d3
“Emma <emmalarsen2704@gmail.com>
2024-12-12 23:24:36 +0100

    forside og kontakt

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,29 @@ export default {
     "./public/**/*.html", // Scanner filer i public-mappen (hvis nødvendigt)
   ],
   theme: {
-    extend: {
-      colors: {
-        dark: "#1a1a1a", // Alternativ farve til mørke sektioner
-        accent: "#f5deb3", // Lys beige farve til highlights
-      },
-      spacing: {
-        128: "32rem", // Custom spacing hvis nødvendigt
+    screens: {
+      sm: "640px",
+      //mobil/lille tablet => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // tablet => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // laptop => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // desktop => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // meget stor skærm => @media (min-width: 1536px) { ... }
+
+      extend: {
+        colors: {
+          dark: "#1a1a1a", // Alternativ farve til mørke sektioner
+          accent: "#f5deb3", // Lys beige farve til highlights
+        },
       },
     },
+    plugins: [],
   },
-  plugins: [],
 };
